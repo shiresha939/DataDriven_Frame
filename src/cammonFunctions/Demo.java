043b2d30508a0b81af1840e5136ae92caf73d347
welcome <welcome@DESKTOP-7QKHQO8>
2023-04-21 22:33:25 +0530

    new class commit

---
+++ src/cammonFunctions/Demo.java
@@ -0,0 +1,27 @@
+package cammonFunctions;
+
+public class Demo {
+	public void add()
+	{
+		int a=456,b=98,c;
+		c=a+b;
+		System.out.println(c);
+	}
+	public void div()
+	{
+		int a=456,b=98,c;
+		c=a/b;
+		System.out.println(c);
+	}
+	public void mul()
+	{
+		int a=456,b=98,c;
+		c=a*b;
+		System.out.println(c);
+	}
+}
+
+		
+	
+
+
